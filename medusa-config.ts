a0055109bb521224663f436484c3fabe6db422d6
Ibukunoluwa Naphtali <ibukunoluwanap@gmail.com>
2025-05-07 20:50:53 +0300

    upd

--- medusa-config.ts
+++ medusa-config.ts
@@ -1,60 +1,53 @@
 import { loadEnv, defineConfig } from "@medusajs/framework/utils";
-
-// Pull in your constants from process.env
 import {
-  NODE_ENV,
-  DATABASE_URL,
-  REDIS_URL,
   ADMIN_CORS,
+  ADMIN_URL,
   AUTH_CORS,
-  STORE_CORS,
-  MEDUSA_BACKEND_URL,
-  JWT_SECRET,
-  JWT_EXPIRES_IN,
   COOKIE_SECRET,
-  S3_FILE_URL,
+  DATABASE_URL,
+  JWT_EXPIRES_IN,
+  JWT_SECRET,
+  NODE_ENV,
+  REDIS_URL,
   S3_ACCESS_KEY_ID,
-  S3_SECRET_ACCESS_KEY,
-  S3_REGION,
   S3_BUCKET,
   S3_ENDPOINT,
-  MEDUSA_ADMIN_BACKEND_URL,
+  S3_FILE_URL,
+  S3_REGION,
+  S3_SECRET_ACCESS_KEY,
+  STORE_CORS,
 } from "./src/lib/constants";
 
-// Load .env
 loadEnv(NODE_ENV || "development", process.cwd());
 
-export default defineConfig({
+module.exports = defineConfig({
   projectConfig: {
     databaseUrl: DATABASE_URL!,
     redisUrl: REDIS_URL!,
     databaseLogging: true,
     databaseDriverOptions: {
       connection: { ssl: { rejectUnauthorized: false } },
-      pool: { min: 0, max: 20, acquireTimeoutMillis: 60000 },
+      pool: {
+        min: 0,
+        max: 20,
+        acquireTimeoutMillis: 60000,
+      },
       idle_in_transaction_session_timeout: 60000,
     },
     http: {
+      storeCors: STORE_CORS!,
       adminCors: ADMIN_CORS!,
       authCors: AUTH_CORS!,
-      storeCors: STORE_CORS!,
-      jwtSecret: JWT_SECRET!,
-      jwtExpiresIn: JWT_EXPIRES_IN!,
-      cookieSecret: COOKIE_SECRET!,
+      jwtSecret: JWT_SECRET || "supersecret",
+      jwtExpiresIn: JWT_EXPIRES_IN,
+      cookieSecret: COOKIE_SECRET || "supersecret",
     },
   },
-
-  // Admin UI settings
   admin: {
     disable: false,
-    // This must point at your API, not at itself
-    backendUrl:
-      MEDUSA_ADMIN_BACKEND_URL || MEDUSA_BACKEND_URL || "http://localhost:9000",
-    // Serving path for the Admin UI (root in PROD)
-    path: NODE_ENV === "development" ? "/app" : "/",
+    backendUrl: ADMIN_URL || "http://localhost:9000/",
+    path: NODE_ENV !== "development" ? "/" : "/app",
   },
-
-  // Your S3 (DigitalOcean Spaces) provider
   modules: [
     {
       resolve: "@medusajs/medusa/file",
